81ec06251c5d24223d01339c425aed78497a3234
Josh Chisholm <joshuachisholm@gmail.com>
2014-10-16 10:20:34 +0100

    gitignore .idea stuff

--- ee/FirebaseClient.m
+++ ee/FirebaseClient.m
@@ -5,32 +5,38 @@
 @implementation FirebaseClient
 
 -(void)saveLinkedInProfileWithId:(NSString*)id andProfile:(NSDictionary*)profile {
-    NSString *profileUrl = [NSString stringWithFormat:@"https://incandescent-inferno-9409.firebaseio.com/users/%@/linkedInProfile", id];
-    Firebase* firebase = [[Firebase alloc] initWithUrl:profileUrl];
+    NSString *path = [NSString stringWithFormat:@"users/%@/linkedInProfile", id];
+    Firebase *firebase = [self firebaseForPath:path];
     [firebase setValue:profile];
 }
 
 - (void)matchesForUser:(NSString *)userId withBlock:(void (^)(NSArray *))block {
-    Firebase *ref = [[Firebase alloc] initWithUrl:[NSString stringWithFormat:@"https://incandescent-inferno-9409.firebaseio.com/matches/%@", userId]];
-
-    FQuery* matchesQuery = [ref queryLimitedToNumberOfChildren:10];
-
-    NSMutableArray *results = [[NSMutableArray alloc] init];
+    NSString *path = [NSString stringWithFormat:@"matches/%@", userId];
+    Firebase *matchesRoot = [self firebaseForPath:path];
+    FQuery* matchesQuery = [matchesRoot queryLimitedToNumberOfChildren:10];
 
     [matchesQuery observeEventType:FEventTypeChildAdded withBlock:^(FDataSnapshot *snapshot) {
-
-        Firebase *theirProfile = [[Firebase alloc] initWithUrl:[NSString stringWithFormat:@"https://incandescent-inferno-9409.firebaseio.com/users/%@/linkedInProfile", snapshot.name]];
-
-        unsigned long matchCount = (unsigned long)[snapshot childrenCount] - 1;
-
-        [theirProfile observeSingleEventOfType:FEventTypeValue withBlock:^(FDataSnapshot *theirProfileSnapshot) {
-            LinkedInProfile *theirLinkedInProfile = [[LinkedInProfile alloc] initWithLinkedInApiUserData:theirProfileSnapshot.value];
-            [results addObject:theirLinkedInProfile];
-            if (results.count == matchCount) {
-                block(results);
-            }
-        }];
+        [self getProfilesForMatches:snapshot block:block];
     }];
+}
 
+- (void)getProfilesForMatches:(FDataSnapshot *)snapshot block:(void (^)(NSArray *))block {
+    NSMutableArray *results = [[NSMutableArray alloc] init];
+    NSString *theirProfilePath = [NSString stringWithFormat:@"users/%@/linkedInProfile", snapshot.name];
+    Firebase *theirProfileFirebase = [self firebaseForPath:theirProfilePath];
+    unsigned long matchCount = (unsigned long)[snapshot childrenCount] - 1;
+    [theirProfileFirebase observeSingleEventOfType:FEventTypeValue withBlock:^(FDataSnapshot *theirProfileSnapshot) {
+        LinkedInProfile *theirLinkedInProfile = [[LinkedInProfile alloc] initWithLinkedInApiUserData:theirProfileSnapshot.value];
+        [results addObject:theirLinkedInProfile];
+        if (results.count == matchCount) {
+            block(results);
+        }
+    }];
+}
+
+- (Firebase *)firebaseForPath:(NSString *)path {
+    NSString *rootUrl = @"https://incandescent-inferno-9409.firebaseio.com/";
+    NSString *fullUrl = [rootUrl stringByAppendingString:path];
+    return [[Firebase alloc] initWithUrl:fullUrl];
 }
 @end
