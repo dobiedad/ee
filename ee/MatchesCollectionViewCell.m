409f56b8f4ebaa866cb32dea54dcd9728d87ea96
Leo Mdivani <leomdiv@gmail.com>
2014-10-21 16:11:04 +0100

    profile in collectioncell

--- ee/MatchesCollectionViewCell.m
+++ ee/MatchesCollectionViewCell.m
@@ -14,12 +14,12 @@
 - (void)loadProfile:(LinkedInProfile *)profile {
     _profile = profile;
     [self.imageView sd_setImageWithURL:[profile pictureURL] placeholderImage:nil];
-    self.imageView.layer.cornerRadius = 60;
-    [imageView setClipsToBounds:YES];
     jobLabel.text =  [_profile companyName], [_profile industry];
     nameLabel.text = [_profile firstName];
     self.imageView.layer.borderColor = [UIColor greenColor].CGColor;
     self.imageView.layer.borderWidth = 1;
+
+    
     
 }
 
