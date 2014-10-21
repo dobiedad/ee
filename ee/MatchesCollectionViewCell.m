1f71b75744f402d17a24dce3982a19e2866ab526
Leo Mdivani <leomdiv@gmail.com>
2014-10-21 11:29:43 +0100

    added name and job label to collectionview

--- ee/MatchesCollectionViewCell.m
+++ ee/MatchesCollectionViewCell.m
@@ -6,12 +6,19 @@
     LinkedInProfile *_profile;
 }
 @synthesize imageView;
+@synthesize jobLabel;
+@synthesize nameLabel;
+
+
 
 - (void)loadProfile:(LinkedInProfile *)profile {
     _profile = profile;
     [self.imageView sd_setImageWithURL:[profile pictureURL] placeholderImage:nil];
     self.imageView.layer.cornerRadius = 60;
     [imageView setClipsToBounds:YES];
+    jobLabel.text =  [_profile companyName], [_profile industry];
+    nameLabel.text = [_profile firstName];
+ 
 }
 
 - (LinkedInProfile *)profile {
