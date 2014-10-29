94ee80e941db9f1f25f3ccb4c02f042c864e77f9
Leo Mdivani <leomdiv@gmail.com>
2014-10-29 13:07:49 +0000

    added chat controller

---
+++ ee/ChatController.h
@@ -0,0 +1,13 @@
+//
+//  ChatController.h
+//  ee
+//
+//  Created by Leo Mdivani on 29/10/2014.
+//  Copyright (c) 2014 Dizzolve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChatController : UIViewController
+
+@end
---
+++ ee/ChatController.m
@@ -0,0 +1,13 @@
+//
+//  ChatController.m
+//  ee
+//
+//  Created by Leo Mdivani on 29/10/2014.
+//  Copyright (c) 2014 Dizzolve. All rights reserved.
+//
+
+#import "ChatController.h"
+
+@implementation ChatController
+
+@end
